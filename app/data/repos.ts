ad82240e4341a03e97fa5a2a446bce207b8b8205
Nikita <nicitaacom@gmail.com>
2025-03-09 12:52:38 +0100

    fix: project links in navbar

--- app/data/repos.ts
+++ app/data/repos.ts
@@ -2,97 +2,97 @@ const repos = [
   {
     id: 1,
     description: "influencers",
-    url: "https://github.com/Nicitaa/MyFirstSite",
+    url: "https://github.com/nicitaacom/MyFirstSite",
   },
   {
     id: 2,
     description: "spaklevka",
-    url: "https://github.com/Nicitaa/MySecondSite",
+    url: "https://github.com/nicitaacom/MySecondSite",
   },
   {
     id: 3,
     description: "bootstrap net ninja",
-    url: "https://github.com/Nicitaa/MyThirdSite",
+    url: "https://github.com/nicitaacom/MyThirdSite",
   },
   {
     id: 4,
     description: "go trip",
-    url: "https://github.com/Nicitaa/MyFourthSite",
+    url: "https://github.com/nicitaacom/MyFourthSite",
   },
   {
     id: 5,
     description: "netflix clone",
-    url: "https://github.com/Nicitaa/MyFifthSite",
+    url: "https://github.com/nicitaacom/MyFifthSite",
   },
   {
     id: 6,
     description: "invoriem",
-    url: "https://github.com/Nicitaa/MySixthSite",
+    url: "https://github.com/nicitaacom/MySixthSite",
   },
   {
     id: 7,
     description: "navbars forms",
-    url: "https://github.com/Nicitaa/MySeventhSite",
+    url: "https://github.com/nicitaacom/MySeventhSite",
   },
   {
     id: 8,
     description: "accordeon pop-up",
-    url: "https://github.com/Nicitaa/MyEighthSite",
+    url: "https://github.com/nicitaacom/MyEighthSite",
   },
   {
     id: 9,
     description: "project",
-    url: "https://github.com/Nicitaa/MyNinethSite",
+    url: "https://github.com/nicitaacom/MyNinethSite",
   },
   {
     id: 10,
     description: "product",
-    url: "https://github.com/Nicitaa/MyTenthSite",
+    url: "https://github.com/nicitaacom/MyTenthSite",
   },
   {
     id: 11,
     description: "githubApi",
-    url: "https://github.com/Nicitaa/MyEleventhSite",
+    url: "https://github.com/nicitaacom/MyEleventhSite",
   },
   {
     id: 12,
     description: "shopping cart",
-    url: "https://github.com/Nicitaa/MyTwelvthSite",
+    url: "https://github.com/nicitaacom/MyTwelvthSite",
   },
   {
     id: 13,
     description: "to-do",
-    url: "https://github.com/Nicitaa/MyirteenthSite",
+    url: "https://github.com/nicitaacom/MyThirteenthSite",
   },
   {
     id: 14,
     description: "portfolio",
-    url: "https://github.com/Nicitaa/14_portfolio",
+    url: "https://github.com/nicitaacom/14_portfolio",
   },
   {
     id: 15,
     description: "HooBank",
-    url: "https://github.com/Nicitaa/15_HooBank",
+    url: "https://github.com/nicitaacom/15_HooBank",
   },
   {
     id: 16,
     description: "gericht-restaurant",
-    url: "https://github.com/Nicitaa/16_gericht-restaurant",
+    url: "https://github.com/nicitaacom/16_gericht-restaurant",
   },
   {
     id: 17,
     description: "messenger clone",
-    url: "https://github.com/nicitaacom/17_messenger-clone",
+    url: "https://github.com/nicitaacomcom/17_messenger-clone",
   },
   {
     id: 18,
     description: "utilities",
-    url: "https://github.com/Nicitaa/18_utilities",
+    url: "https://github.com/nicitaacom/18_utilities",
   },
   {
     id: 19,
     description: "spotify clone",
-    url: "https://github.com/Nicitaa/19_spotify-clone",
+    url: "https://github.com/nicitaacom/19_spotify-clone",
   },
   {
     id: 20,
@@ -102,12 +102,12 @@ const repos = [
   {
     id: 21,
     description: "auth-form (FullStack)",
-    url: "https://github.com/Nicitaa/21_auth-form",
+    url: "https://github.com/nicitaacom/21_auth-form",
   },
   {
     id: 22,
     description: "aer",
-    url: "https://github.com/Nicitaa/22_aer",
+    url: "https://github.com/nicitaacom/22_aer",
   },
   {
     id: 23,
@@ -117,12 +117,12 @@ const repos = [
   {
     id: 24,
     description: "dashboard MUI",
-    url: "https://github.com/Nicitaa/24_dashboard-mui",
+    url: "https://github.com/nicitaacom/24_dashboard-mui",
   },
   {
     id: 26,
     description: "hot delivery",
-    url: "https://www.google.com/search?sca_esv=8d36031256b28c5e&sca_upv=1&sxsrf=ADLYWIJ67tpxRTKEYP9GaVnpZo2be_3N3Q:1726807004449&q=closed+source&source=lnms&fbs=AEQNm0Aa4sjWe7Rqy32pFwRj0UkWd8nbOJfsBGGB5IQQO6L3J_86uWOeqwdnV0yaSF-x2jqw-AzvpDFRWNmLZKilfTrfn09q0QL89IE2BK9wqCaoIVFOCV1aoDeP5tw4R7IXvsEru5h7CjL6p8iEdExKawty8Ih9BaQ4THdbQdc9OzGogmA3n2ZhZZDIZC9rq5LYItoW5Tge&sa=X&ved=2ahUKEwjc7uGw2dCIAxW00AIHHclmGuIQ0pQJegQIDRAB&biw=1920&bih=958&dpr=1",
+    url: "https://hot-delivery.net/",
   },
   {
     id: 27,
