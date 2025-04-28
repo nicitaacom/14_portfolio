58ad7c4beec31a668547db19a13fa1706f9db2ad
Nikita <nicitaacom@gmail.com>
2025-04-28 12:45:21 +0200

    upd: summary link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -20,7 +20,7 @@ export function Footer() {
           LinkedIn <AiFillLinkedin />
         </Button>
         <Button
-          href="https://drive.google.com/file/d/1m9v_vOA3fCy_mrF2MEimIyIyVI8FiH4W/view?usp=sharing"
+          href="https://drive.google.com/file/d/1w0QnKXQ7f7mVrmYb_EdLMfVsBz2Nda_3/view?usp=sharing"
           target="_blank">
           Summary <BsFiletypePdf />
         </Button>
