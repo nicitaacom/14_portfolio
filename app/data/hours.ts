7999afb29d8ccf53dc85fb29372946272c706d62
Nikita <nicitaacom@gmail.com>
2025-08-04 12:10:36 +0200

    2025-07-28 until 2025-08-03

--- app/data/hours.ts
+++ app/data/hours.ts
@@ -1,15 +1,15 @@
-// 2025-07-21 until 2025-07-27
+// 2025-07-28 until 2025-08-03
 
 // if you see an email from waka time - update it and rest of skills
 export const hours = {
-  htmlcss: 4967,
+  htmlcss: 4989,
   vite: 1163,
-  next: 3420,
-  typescript: 4468,
+  next: 3442,
+  typescript: 4490,
   other: {
-    supabase: 287,
-    zustand: 303,
-    photoshop: 163,
+    supabase: 290,
+    zustand: 304,
+    photoshop: 164,
     figma: 153,
     devops: 151,
   },
