577243c481d06ef53973569dd20b11de080d7e91
Nikita <nicitaacom@gmail.com>
2025-09-26 19:12:00 +0200

    upd: CV link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -20,7 +20,7 @@ export function Footer() {
           LinkedIn <AiFillLinkedin />
         </Button>
         <Button
-          href="https://drive.google.com/file/d/1oGjYJEbqq4kUVSgq4P4D8J71r_tOHWMB/view?usp=sharing"
+          href="https://drive.google.com/file/d/1KQG3sefloXLON0l2QTXpXQLpMTmyxO3b/view?usp=sharing"
           target="_blank">
           Summary <BsFiletypePdf />
         </Button>
