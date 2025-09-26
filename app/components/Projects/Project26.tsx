52e8fc9656bbf4e8aaceccc56a11df2150169769
Nikita <nicitaacom@gmail.com>
2025-09-26 14:13:38 +0200

    upd: link to hot delivery with utm

--- app/components/Projects/Project26.tsx
+++ app/components/Projects/Project26.tsx
@@ -6,7 +6,7 @@ export default function Project26({ openModal }: { openModal: () => void }) {
       stack="Next, TypeScript, Tailwind, Supabase, OpenAI, AWS, GCP"
       date="11.2023 - 01.2023 (~643h)"
       openMoreInfoModal={openModal}
-      siteUrl="https://hot-delivery.net/"
+      siteUrl="https://hot-delivery.net/?utm_source=portfolio&utm_medium=website&utm_campaign=personal_portfolio&utm_content=homepage"
       figmaUrl="https://www.figma.com/file/naTRYx4iTl8QhsEM584Nvv/26_hot-delivery?type=design&node-id=136-97&mode=design&t=mbTnMKHyu81vCueg-0"
       youTubeEmbedPreview="https://www.youtube.com/embed/7WX6zA8qgp4?si=8zNxCVLensQBIRbJ"
     />
