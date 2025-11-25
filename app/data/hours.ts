03cad4cc14be85d18cc44c17b6efa26078420e9c
Nikita <nicitaacom@gmail.com>
2025-11-25 14:46:20 +0100

    2025-11-17 until 2025-11-23
    interesting how it scipped 1-2 weeks
    1 because email
    2 because commented line

--- app/data/hours.ts
+++ app/data/hours.ts
@@ -1,15 +1,15 @@
-// 2025-10-27 until 2025-11-02
+// 2025-11-17 until 2025-11-23
 
 // if you see an email from waka time - update it and rest of skills
 export const hours = {
-  htmlcss: 5341,
+  htmlcss: 5373,
   vite: 1163,
-  next: 3791,
-  typescript: 4860,
+  next: 3823,
+  typescript: 4892,
   other: {
-    supabase: 332,
-    zustand: 328,
-    photoshop: 170,
+    supabase: 333,
+    zustand: 330,
+    photoshop: 171,
     figma: 153,
     devops: 153,
   },
