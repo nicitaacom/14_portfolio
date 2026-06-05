f9ba18476b4895969eb98989b705fc9a19e03ef7
Nikita <nicitaacom@gmail.com>
2026-06-05 16:04:12 +0200

    fix: use MSK time in isDisabledFn

--- app/[locale]/(site)/functions/isDisabledFn.ts
+++ app/[locale]/(site)/functions/isDisabledFn.ts
@@ -1,13 +1,12 @@
-import moment from "moment"
+import moment from "moment-timezone"
 
 export function isDisabledFn(time: string, targetDate: Date): boolean {
-  const now = new Date()
-  const currentHour = now.getHours()
-  const currentMinute = now.getMinutes()
+  const currentMoment = moment().tz("Europe/Moscow")
+  const currentHour = currentMoment.hours()
+  const currentMinute = currentMoment.minutes()
 
   const [hour, minute] = time.split(":").map(Number)
   const selectedMoment = moment(targetDate).tz("Europe/Moscow").startOf("day")
-  const currentMoment = moment().tz("Europe/Moscow")
 
   if (selectedMoment.isSame(currentMoment, "day")) {
     return hour < currentHour || (hour === currentHour && minute < currentMinute)
