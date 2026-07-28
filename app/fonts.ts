f76741317d4d41a611b1b6204d8b1363cc9a96d5
Nikita <nicitaacom@gmail.com>
2026-07-28 17:37:43 +0200

    chore: new font Special_Elite

---
+++ app/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Special_Elite } from "next/font/google"
+
+export const inter = Inter({ subsets: ["latin"], variable: "--font-inter", display: "swap" })
+export const specialElite = Special_Elite({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-typewriter",
+  display: "swap",
+})
