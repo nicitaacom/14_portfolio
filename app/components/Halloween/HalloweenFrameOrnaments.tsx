403ac4c7b17526e13cf8776ce84a86d27fc73237
Nikita <nicitaacom@gmail.com>
2026-07-29 01:10:47 +0200

    style(fix): skull svg looked alien

--- app/components/Halloween/HalloweenFrameOrnaments.tsx
+++ app/components/Halloween/HalloweenFrameOrnaments.tsx
@@ -31,12 +31,14 @@ export function HalloweenFrameOrnaments({ variant }: HalloweenFrameOrnamentsProp
         className="halloween-frame-web"
         d="M22 105L146 22M22 105L212 112M22 105L106 211M56 82C89 91 115 105 137 129M86 61C124 70 157 89 183 115M944 82C911 91 885 105 863 129M914 61C876 70 843 89 817 115"
       />
+      {/* Stencil skull: wide cranium pinching at the temples, flared cheekbones and a broad grin.
+          The first path is the bone silhouette, every path after it is a dark cut-out. */}
       <g className="halloween-frame-skull" transform={skullTransform}>
-        <path d="M45 5C72 5 91 24 91 51C91 70 81 85 66 92V111H26V92C10 84 1 69 1 51C1 24 19 5 45 5Z" />
-        <ellipse cx="28" cy="50" rx="12" ry="15" />
-        <ellipse cx="63" cy="50" rx="12" ry="15" />
-        <path d="M45 62L36 76H54L45 62Z" />
-        <path d="M28 92V110M39 91V111M51 91V111M62 92V110" />
+        <path d="M50 1C72 1 89 15 92 35C94 45 92 52 89 57C93 61 94 67 91 72C88 78 82 81 76 83C74 92 70 100 63 106C59 109 54 110 50 110C46 110 41 109 37 106C30 100 26 92 24 83C18 81 12 78 9 72C6 67 7 61 11 57C8 52 6 45 8 35C11 15 28 1 50 1Z" />
+        <path d="M13 42C21 34 38 34 44 43L47 56C45 62 34 65 25 62C16 59 10 50 13 42Z" />
+        <path d="M87 42C79 34 62 34 56 43L53 56C55 62 66 65 75 62C84 59 90 50 87 42Z" />
+        <path d="M50 62C47 68 42 73 41 79C40 84 44 87 48 85C49 84 50 83 50 82C50 83 51 84 52 85C56 87 60 84 59 79C58 73 53 68 50 62Z" />
+        <path d="M27 86H73V89H27ZM27 96H73V99H27ZM33 89H35V96H33ZM39 89H41V96H39ZM45 89H47V96H45ZM51 89H53V96H51ZM57 89H59V96H57ZM63 89H65V96H63ZM36 99H38V106H36ZM42 99H44V106H42ZM48 99H50V106H48ZM54 99H56V106H54ZM60 99H62V106H60Z" />
       </g>
       <g className="halloween-frame-pumpkin halloween-frame-pumpkin-left" transform={leftPumpkinTransform}>
         <ellipse cx="55" cy="48" rx="52" ry="43" />
