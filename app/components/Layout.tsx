096071e41fc8bcb68c1e7ee570461200eb0e0dfa
Nikita <nicitaacom@gmail.com>
2026-07-28 23:59:06 +0200

    fix: page scroll trapped in Layout

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -20,7 +20,7 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-[calc(100vh-72px)] overflow-x-hidden overflow-y-auto transition-colors duration-300 pt-[62px]">
+    <div className="min-h-[calc(100vh-72px)] overflow-x-clip transition-colors duration-300 pt-[62px]">
       {children}
       <ModalMoreInfoNDA />
       <ModalMoreInfo15 />
