4cc33ea9610e019f0731cf6c6812c1272d50dc3e
Nikita <nicitaacom@gmail.com>
2026-08-01 20:47:41 +0200

    style(fix): isGMLive dot overflow

--- app/[locale]/(site)/appointment/components/GMLiveCheckbox.tsx
+++ app/[locale]/(site)/appointment/components/GMLiveCheckbox.tsx
@@ -24,8 +24,8 @@ export function GMLiveCheckbox({ isGMLive }: { isGMLive: boolean }) {
   return (
     <label
       className={twMerge(
-        `relative w-[36px] h-[20px] border-[3px] hover:brightness-75 transition-all duration-300 
-         rounded-[12px] cursor-pointer mx-1 flex`,
+        `relative w-[36px] h-[20px] border-[3px] hover:brightness-75 transition-all duration-300
+         rounded-[12px] cursor-pointer mx-1 flex overflow-hidden`,
         isLoading && "opacity-50 cursor-default pointer-events-none flex justify-center items-center",
       )}>
       <input
@@ -36,10 +36,10 @@ export function GMLiveCheckbox({ isGMLive }: { isGMLive: boolean }) {
         onChange={toggleAcceptMessagesFn}
       />
       <span
-        className={`absolute rounded-[50%]
-      w-[10px] h-[10px] translate-x-[20%] translate-y-[20%] bg-danger transition-all duration-500
+        className={`absolute left-[2px] top-[2px] rounded-[50%]
+      w-[10px] h-[10px] bg-danger transition-all duration-500
       before:absolute before:content-['']
-      peer-checked/input:translate-x-[180%] peer-checked/input:bg-success
+      peer-checked/input:translate-x-[16px] peer-checked/input:bg-success
       `}
       />
     </label>
