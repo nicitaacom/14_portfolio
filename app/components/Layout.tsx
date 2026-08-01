0cae76fa4206852858d4166df13bd2eaea6e9330
Nikita <nicitaacom@gmail.com>
2026-08-01 21:09:23 +0200

    fix: appointment 5px scroll from Layout floor

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -21,7 +21,7 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-[calc(100vh-72px)] overflow-x-clip transition-colors duration-300 pt-[62px]">
+    <div className="site-shell min-h-[calc(100vh-72px)] overflow-x-clip transition-colors duration-300 pt-[62px]">
       {children}
       <ModalMoreInfoNDA />
       <ModalMoreInfoNexgem />
